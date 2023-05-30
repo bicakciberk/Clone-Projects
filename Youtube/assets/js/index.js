aad7279c5d8f1ff581340612bc397833f5b59f56
Berk Bıçakcı <120296952+bicakciberk@users.noreply.github.com>
2023-05-30 21:50:55 +0300

    Add files via upload

---
+++ Youtube/assets/js/index.js
@@ -0,0 +1,173 @@
+/*nav, side-bar*/
+const menuBtn = document.querySelector('.menu');
+const sideText = document.querySelectorAll('.side-text');
+const sideBar = document.querySelector('#side-bar');
+const thisHide = document.querySelectorAll('.this-hide');
+const main = document.querySelector('#main');
+const windowWidth = window.innerWidth;
+const navbar = document.querySelector('#nav-bar');
+const cardContainer = document.querySelector('.card-container');
+const videoContainer = document.querySelector('.video-container');
+
+
+function hideText() {
+    menuBtn.addEventListener('click', function () {
+        if (windowWidth > 581) {
+            sideText.forEach((text) => {
+                text.classList.toggle('hide');
+                sideBar.classList.toggle('width')
+            })
+
+            thisHide.forEach((data) => {
+                data.classList.toggle('hidden')
+            })
+
+            main.classList.toggle('width')
+        } else {
+
+        }
+
+    })
+}
+
+hideText()
+
+/*profile*/
+const profile = document.querySelector('.profile');
+const pp = document.querySelector('.avatar');
+
+function showProfile() {
+    pp.addEventListener('click', function () {
+        profile.classList.toggle('active');
+    });
+}
+showProfile();
+
+/*theme*/
+const defaultBtn = document.querySelector('#default-btn') //bilgisayarın kullandığı tema
+const lightBtn = document.querySelector('#light-btn') //açık tema 
+const darkBtn = document.querySelector('#dark-btn') //karanlık tema
+const themeWrite = document.querySelector('#theme-span'); //temadan mesela karanlık temaya basıldığında profile yerindeki span'ın değiştirilmesini sağlar
+
+
+const theme = document.querySelector('.theme') // temaların bulunduğu container
+const themeBtn = document.querySelector('#themeBTN') // profile kısmındaki tema yerini açmayı sağlayan yer (ul li)
+
+const themeHeader = document.querySelector('.theme-header')
+
+
+
+function selectCount() {
+    defaultBtn.addEventListener('click', function () {
+        defaultBtn.classList.add('active');
+        lightBtn.classList.remove('active')
+        darkBtn.classList.remove('active')
+        themeWrite.textContent = 'Default'
+    })
+
+    lightBtn.addEventListener('click', function () {
+        lightBtn.classList.add('active');
+        defaultBtn.classList.remove('active')
+        darkBtn.classList.remove('active')
+        themeWrite.textContent = 'Açık'
+    })
+
+
+    darkBtn.addEventListener('click', function () {
+        darkBtn.classList.add('active');
+        defaultBtn.classList.remove('active')
+        lightBtn.classList.remove('active')
+        themeWrite.textContent = 'Karanlık'
+
+    })
+}
+
+selectCount()
+
+function openThemePage() {
+    themeBtn.addEventListener('click', function () {
+        theme.classList.add('active')
+        profile.classList.remove('active')
+    })
+
+    themeHeader.addEventListener('click', function () {
+        theme.classList.remove('active')
+        profile.classList.add('active')
+    })
+
+    pp.addEventListener('click',function(){
+        theme.classList.remove('active')
+    })
+}
+
+openThemePage()
+
+
+const body = document.querySelector('body');
+const ytText = document.querySelector('.yt-text');
+const navSearch = document.querySelector('#nav-search');
+const ytMic = document.querySelector('.yt-mic')
+
+function changeLight() {
+    lightBtn.addEventListener('click', function () {
+        body.classList.add('light');
+        navbar.classList.add('light');
+        sideBar.classList.add('light');
+        profile.classList.add('light');
+        theme.classList.add('light');
+        ytText.classList.add('light');
+        sideBar.classList.add('light');
+        menuBtn.classList.add('light');
+        navSearch.classList.add('light');
+        sideBar.classList.add('light');
+        ytMic.classList.add('light');
+        main.classList.add('light');
+        videoContainer.classList.add('light');
+        theme.classList.add('light');
+        cardContainer.classList.add('light')
+    })
+}
+
+function changeDark() {
+    darkBtn.addEventListener('click', function () {
+        body.classList.remove('light');
+        navbar.classList.remove('light');
+        sideBar.classList.remove('light');
+        profile.classList.remove('light');
+        theme.classList.remove('light');
+        ytText.classList.remove('light');
+        sideBar.classList.remove('light');
+        menuBtn.classList.remove('light');
+        navSearch.classList.remove('light');
+        sideBar.classList.remove('light');
+        ytMic.classList.remove('light');
+        main.classList.remove('light');
+        videoContainer.classList.remove('light');
+        theme.classList.remove('light');
+        cardContainer.classList.remove('light')
+    })
+}
+
+function setLocalStorage(){
+    if(body.classList.contains('light')){
+        localStorage.setItem('theme', 'light')
+    } else{
+        localStorage.setItem('theme', 'dark')
+    }
+}
+
+function getLocalStorage(){
+    if(localStorage.getItem('theme') === 'light'){
+        body.classList.add('light')
+    } else{
+        body.classList.remove('light')
+    }
+}
+
+setLocalStorage()
+getLocalStorage()
+
+
+
+changeLight()
+changeDark()
